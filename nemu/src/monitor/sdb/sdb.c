b0fa555e7480049ba447fad5e7bb3f57d7502fcf
tracer-ics2022 <tracer@njuics.org>
2022-09-14 16:31:43 +0800

    >  compile NEMU 211300005 李琪杰 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:31:43 up 11 min,  1 user,  load average: 0.00, 0.10, 0.08

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -123,7 +123,7 @@ static int cmd_p(char *args){
     return 0;
   }
   word_t result = expr(e, &success);
-  if(success) printf("Result:\ns: %d\nu: %u\nh: %x\n", result, result, result);
+  if(success) printf("%u\n", result);
   else printf("Calculation failed.\n");
   return 0;
 }
