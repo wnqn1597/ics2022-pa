fad04eaf96cb0d46d5173189355697e0f3fb17c6
tracer-ics2022 <tracer@njuics.org>
2022-09-18 21:05:31 +0800

    >  compile NEMU 211300005 李琪杰 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:05:31 up  5:45,  1 user,  load average: 0.23, 0.20, 0.12

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -16,6 +16,7 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint64_t sech = inl(RTC_ADDR+4);
   uint64_t current_us = (sech << 32) | secl;
   uptime->us = current_us - boot_us;
+  printf("ret=%d\n", current_us-boot_us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
