332a568ce25c9d8ec45fcbfa4ae52bf8acf19ba9
tracer-ics2022 <tracer@njuics.org>
2022-09-19 10:45:58 +0800

    >  compile NEMU 211300005 李琪杰 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:45:58 up 8 min,  1 user,  load average: 0.25, 0.38, 0.25

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,53 +5,196 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-char int_to_char(unsigned n) {
-  if(n >= 0 && n < 10) return (char)('0' + n);
-  else if(n >= 10 && n < 16) return (char)('0' + n + 39);
-  else return '#';
+int     BIT64   = 0x1,
+        BIT128  = 0x2,
+        SPACE   = 0x4,
+        SPECIAL = 0x8,
+        ZEROPAD = 0x10,
+        SMALL   = 0x20,
+        SIGN    = 0x40;
+
+int is_digit(char ch){
+    return ch >= '0' && ch <= '9';
+}
+
+int skip_atoi(const char **s){
+    int i = 0;
+    while(is_digit(**s)) i = i * 10 + *((*s)++) - '0';
+    return i;
+}
+
+char * number(char * str, int64_t num, int base, int size, int precision, int type)
+{
+    char c,sign,tmp[36];
+    const char *digits="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int i;
+
+    if (type&SMALL) digits="0123456789abcdefghijklmnopqrstuvwxyz";
+    if (base<2 || base>36) return 0;
+    c = (type & ZEROPAD) ? '0' : ' ' ;
+    if (!(type&BIT64)) num = (int32_t)num;
+    if ((type&SIGN) && num<0) {
+        sign='-';
+        num = -num;
+    }
+    if (sign) size--;
+    if (type&SPECIAL){
+        if (base==16) size -= 2;
+        else if (base==8) size--;
+    }
+    i=0;
+    if (num==0) tmp[i++]='0';
+    else {
+        while (num!=0) {
+            tmp[i++]=digits[num % base];
+            num /= base;
+        }
+    }
+
+    if (i>precision) precision=i;
+    size -= precision;
+    if (!(type&(ZEROPAD)))
+        while(size-->0)
+            *str++ = ' ';
+    if (sign)
+        *str++ = sign;
+    if (type&SPECIAL){
+        if (base==8) *str++ = '0';
+        else if (base==16) {
+            *str++ = '0';
+            *str++ = digits[33];
+        }
+    }
+    while(size-->0)
+        *str++ = c;
+    while(i<precision--)
+        *str++ = '0';
+    while(i-->0)
+        *str++ = tmp[i];
+    while(size-->0)
+        *str++ = ' ';
+    return str;
 }
 
 int vsprintf(char *buf, const char *fmt, va_list args) {
-    char *str;
+    int len;
+    int i;
+    char * str;
+    char *s;
+    int *ip;
 
-    for (str = buf; *fmt != '\0'; fmt++) {
-        if (*fmt != '%') {
+    int flags;
+
+    int field_width;
+    int precision;
+
+    for (str=buf ; *fmt ; fmt++) {
+        if (*fmt != '%') { // 非%就复制
             *str++ = *fmt;
             continue;
         }
-        fmt++;
-        if(*fmt == 'd' || *fmt == 'x' || *fmt == 'p') {
-	  unsigned scale;
-	  switch(*fmt){
-	    case 'x': 
-	    case 'p': scale = 16;break;
-	    case 'd': scale = 10;break;
-	    default: scale = 10;
-	  }
-	    unsigned long arg = va_arg(args, unsigned long);
-	    unsigned long long b = 1;
-	    unsigned c = 0;
-	    while(arg / b != 0) {
-	      b *= scale;
-	      c++;
-	    }
-	    unsigned a = c - 1;
-	    while(arg > 0) {
-	      unsigned r = arg % scale;
-	      *(str+a) = int_to_char(r);
-	      arg /= scale;
-	      a--;
-	    }
-	    str += c;
-        }else if(*fmt == 's') {
-            char *arg = va_arg(args, char*);
-            strcpy(str, arg);
-            str += strlen(arg);
-        }
-    } /* end of for (str = buf; *fmt; fmt++) */
 
+
+        flags = 0;
+        repeat:
+        fmt++; // 到%后一个
+        switch (*fmt) {
+            case ' ': flags |= SPACE; goto repeat;
+            case '#': flags |= SPECIAL; goto repeat;
+            case '0': flags |= ZEROPAD; goto repeat;
+        }
+
+
+        field_width = -1;
+        if (is_digit(*fmt))
+            field_width = skip_atoi(&fmt);
+        else if (*fmt == '*') {
+            field_width = va_arg(args, int);
+        }
+
+
+        precision = -1;
+        if (*fmt == '.') {
+            ++fmt;
+            if (is_digit(*fmt)) precision = skip_atoi(&fmt);
+            else if (*fmt == '*') precision = va_arg(args, int);
+            if (precision < 0) precision = 0;
+        }
+
+
+        if (*fmt == 'l') {
+            fmt++;
+        }
+        if(*fmt == 'l') {
+            flags |= BIT64;
+            fmt++;
+        }
+
+        switch (*fmt) {
+            case 'c':
+                while (--field_width > 0) *str++ = ' ';
+                *str++ = (char)va_arg(args, int);
+                while (--field_width > 0) *str++ = ' ';
+                break;
+
+            case 's':
+                s = va_arg(args, char*);
+                len = (int)strlen(s);
+                if (precision < 0)
+                    precision = len;
+                else if (len > precision)
+                    len = precision;
+
+                while (len < field_width--)
+                    *str++ = ' ';
+                for (i = 0; i < len; ++i)
+                    *str++ = *s++;
+                while (len < field_width--)
+                    *str++ = ' ';
+                break;
+
+            case 'o':
+                if(flags & BIT64)  str = number(str, va_arg(args, uint64_t), 8, field_width, precision, flags);
+                else               str = number(str, va_arg(args, uint32_t), 8, field_width, precision, flags);
+                break;
+
+            case 'p':
+                if (field_width == -1) {
+                    field_width = 8;
+                    flags |= ZEROPAD;
+                }
+                str = number(str, (uint64_t)(uintptr_t) va_arg(args, void *), 16, field_width, precision, flags);
+                break;
+
+            case 'x':
+                flags |= SMALL;
+            case 'X':
+                if(flags & BIT64)   str = number(str, va_arg(args, uint64_t), 16, field_width, precision, flags);
+                else                str = number(str, va_arg(args, uint32_t), 16, field_width, precision, flags);
+                break;
+
+            case 'd':
+            case 'i':
+                flags |= SIGN;
+            case 'u':
+                if(flags & BIT64)   str = number(str, va_arg(args, uint64_t), 10, field_width, precision, flags);
+                else                str = number(str, va_arg(args, uint32_t), 10, field_width, precision, flags);
+                break;
+
+            case 'n':
+                ip = va_arg(args, int *);
+                *ip = (str - buf);
+                break;
+
+            default:
+                if (*fmt != '%') *str++ = '%';
+                if (*fmt) *str++ = *fmt;
+                else --fmt;
+                break;
+        }
+    }
     *str = '\0';
-    return str - buf;
+    return str-buf;
 }
 
 int sprintf(char *buf, const char *fmt, ...) {
