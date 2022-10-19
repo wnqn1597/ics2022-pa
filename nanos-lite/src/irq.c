c0eae966aabcaffbea653e71f7d8ca24dfa79c83
tracer-ics2022 <tracer@njuics.org>
2022-10-19 18:43:56 +0800

    >  compile NEMU 211300005 李琪杰 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:43:56 up 24 min,  1 user,  load average: 0.52, 0.19, 0.12

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,13 +1,16 @@
 #include <common.h>
 
+void do_syscall(Context *c);
+Context* schedule(Context *prev);
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 		case EVENT_YIELD: c->mepc += 4; Log("YIELD"); break;
 		case EVENT_IRQ_TIMER: break;
-		case EVENT_SYSCALL: printf("sys\n");c->mepc += 4; break;
+		case EVENT_SYSCALL: c->mepc += 4; break;
     default: panic("Unhandled event ID = %d", e.event);
   }
-
+	do_syscall(c);
   return c;
 }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,32 @@
 #include <common.h>
 #include "syscall.h"
+
+void sys_exit(Context *c) {
+	halt(c->GPRx);
+}
+
+void sys_yield(Context *c) {
+	yield();
+	c->GPRx = 0;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+	a[1] = c->GPR2;
+	a[2] = c->GPR3;
+	a[3] = c->GPR4;
   switch (a[0]) {
+		case SYS_exit: sys_exit(c); break;
+		case SYS_yield: sys_yield(c); break;
+		//case SYS_open: sys_open(c, (char*)a[1], a[2], a[3]); break;
+		//case SYS_read: sys_read(c, a[1], (void*)a[2], a[3]); break;
+		//case SYS_write: sys_write(c, a[1], (void*)a[2], a[3]); break;
+		//case SYS_close: sys_close(c, a[1]); break;
+		//case SYS_lseek: sys_lseek(c, a[1], a[2], a[3]); break;
+		//case SYS_brk: sys_brk(c, a[1]); break;
+		//case SYS_execve: sys_execve(c, (char*)a[1], (char**)a[2], (char**)a[3]); break;
+		//case SYS_gettimeofday: sys_gettimeofday(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
