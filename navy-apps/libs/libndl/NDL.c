c3e264c4da442b7b229bc5db8bd725dc4177b8b7
tracer-ics2022 <tracer@njuics.org>
2022-11-03 14:11:28 +0800

    >  compile NEMU 211300005 李琪杰 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:11:28 up  1:24,  1 user,  load average: 0.08, 0.05, 0.06

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -4,25 +4,41 @@
 #include <string.h>
 #include <unistd.h>
 
+#define CENTER 1
+
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
+static int fb_w = 0, fb_h = 0, fb_x = 0, fb_y = 0;
+
+static const char *evFileName = "/dev/events";
+static const char *fbFileName = "/dev/fb";
+static const char *disFileName = "/proc/dispinfo";
 
 uint32_t NDL_GetTicks() {
   return (_syscall_(19, 0, 0, 0) / 1000);
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  return 0;
+  int fd = _syscall_(2, (intptr_t)evFileName, 0, 0);
+  return _syscall_(3, fd, (intptr_t)buf, len);
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
-  if (getenv("NWM_APP")) {
+//  if (getenv("NWM_APP")) {
     int fbctl = 4;
     fbdev = 5;
-    screen_w = *w; screen_h = *h;
+
     char buf[64];
-    int len = sprintf(buf, "%d %d", screen_w, screen_h);
+    if(*w == 0 && *h == 0) {
+      *w = screen_w; *h = screen_h;
+    }
+    fb_w = *w; fb_h = *h;
+    if(CENTER){
+      fb_x = (screen_w - fb_w) / 2;
+      fb_y = (screen_h - fb_h) / 2;
+    }
+    int len = sprintf(buf, "%d %d", *w, *h);
     // let NWM resize the window and create the frame buffer
     write(fbctl, buf, len);
     while (1) {
@@ -33,10 +49,17 @@ void NDL_OpenCanvas(int *w, int *h) {
       if (strcmp(buf, "mmap ok") == 0) break;
     }
     close(fbctl);
-  }
+//  }
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+  uint32_t offset = (fb_y + y) * screen_w + (fb_x + x);
+  int fd = _syscall_(2, (intptr_t)fbFileName, 0, 0);
+  _syscall_(8, fd, offset, SEEK_SET);
+  for(int i = 0; i < h; i++) {
+    _syscall_(4, fd, pixels+i*w, w);
+    _syscall_(8, fd, screen_w - w, SEEK_CUR);
+  }
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
@@ -54,13 +77,27 @@ int NDL_QueryAudio() {
 }
 
 int NDL_Init(uint32_t flags) {
-  printf("NDL_Init\n");
-  if (getenv("NWM_APP")) {
-  	printf("INTO GETENV\n");
+//  if (getenv("NWM_APP")) {
     evtdev = 3;
-  }
+    
+    char buf[10];
+
+    int fd = _syscall_(2, (intptr_t)disFileName, 0, 0);
+    _syscall_(3, fd, (intptr_t)buf, 20);
+    
+    screen_w = atoi(buf);
+    int i;
+    for(i = 0; buf[i] != '\n'; i++);
+    i++;
+    screen_h = atoi(buf+i);
+//  }
   return 0;
 }
 
 void NDL_Quit() {
+  evtdev = -1;
+  fbdev = -1;
+  screen_w = 0, screen_h = 0;
+  fb_w = 0, fb_h = 0, fb_x = 0, fb_y = 0;
+  printf("NDL Quit.\n");
 }
