b30398c2ef31229b0484d7881a6832da03a324c9
tracer-ics2022 <tracer@njuics.org>
2022-11-03 22:17:25 +0800

    >  compile NEMU 211300005 李琪杰 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:17:25 up  2:36,  1 user,  load average: 0.84, 0.46, 0.23

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -35,10 +35,8 @@ static void sh_handle_cmd(const char *cmd) {
 		return;
 	}
 	char *argv[] = {buf, 0};
-	execve(buf, argv, NULL);
+	//execve(buf, argv, NULL);
 	execvp(buf, argv);
-	char *path = NULL;
-	if((path == getenv("PATH"))) printf("PATH=%s\n", path);
 	sh_printf("%s: file not found\n", buf);
 }
 
