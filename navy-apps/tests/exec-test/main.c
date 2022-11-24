3fb3419fbf6a464fa0163f39eeb9040b9146ada9
tracer-ics2022 <tracer@njuics.org>
2022-11-24 19:46:46 +0800

    >  compile NEMU 211300005 李琪杰 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:46:46 up  1:19,  1 user,  load average: 0.22, 0.18, 0.12

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -4,6 +4,10 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+	printf("argc=%d\n", argc);
+	for(int i = 0; i < argc; i++) {
+		printf("%s\n", argv[i]);
+	}
   int n = (argc >= 2 ? atoi(argv[1]) : 1);
   printf("%s: argv[1] = %d\n", argv[0], n);
 
