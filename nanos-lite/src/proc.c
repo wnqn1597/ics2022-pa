056fc617e74045b5eaf2fd585d52d0c065581286
tracer-ics2022 <tracer@njuics.org>
2022-11-25 08:29:40 +0800

    >  compile NEMU 211300005 李琪杰 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:29:40 up 4 min,  1 user,  load average: 0.17, 0.23, 0.11

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -86,10 +86,11 @@ void context_uload(PCB *this_pcb, const char *filename, char* const argv[], char
 
 void init_proc() {
 
-	char *argv[] = {"/bin/pal", NULL};
+	//char *argv[] = {"/bin/pal", NULL};
 
 	context_kload(&pcb[0], hello_fun, 1);
-	context_uload(&pcb[1], "/bin/pal", argv, NULL);
+	context_kload(&pcb[1], hello_fun, 2);
+	//context_uload(&pcb[1], "/bin/pal", argv, NULL);
 
 	switch_boot_pcb();
   // load program here
